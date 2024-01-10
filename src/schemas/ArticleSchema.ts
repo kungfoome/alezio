8b0ae5e13cd2a68b876a202ee28658ce48c714bd
kungfoome <421589+kungfoome@users.noreply.github.com>
2024-01-09 19:09:21 -0700

    Renamed ArticleFeatureImage to ArticleImage and added more options to use for featured images and hero images

--- src/schemas/ArticleSchema.ts
+++ src/schemas/ArticleSchema.ts
@@ -1,9 +1,41 @@
 import { z, type SchemaContext } from 'astro:content';
 
+export enum Blur {
+    None = "none",
+    SM   = "sm",
+    MD   = "md",
+    LG   = "lg",
+    XL   = "xl",
+}
+
+export enum ImageSize {
+    XS = 3,
+    SM = 5,
+    MD = 8,
+    LG = 13,
+    XL = 21,
+}
+
 export const imageSchema = ({ image }: SchemaContext) =>
     z.object({
         src: image(),
         alt: z.string(),
+        blur: z.enum([Blur.None, Blur.SM, Blur.MD, Blur.LG, Blur.XL]).optional(),
+        imageSizeMobile: z.union([
+            z.literal(ImageSize.XS),
+            z.literal(ImageSize.SM),
+            z.literal(ImageSize.MD),
+            z.literal(ImageSize.LG),
+            z.literal(ImageSize.XL)]
+        ).optional(),
+        imageSizeDesktop: z.union([
+            z.literal(ImageSize.XS),
+            z.literal(ImageSize.SM),
+            z.literal(ImageSize.MD),
+            z.literal(ImageSize.LG),
+            z.literal(ImageSize.XL)]
+        ).optional(),
+        ratio: z.number().optional(),
     });
 
 export const articleSchema = ({ image }: { image: any }) => z.object({
@@ -16,5 +48,5 @@ export const articleSchema = ({ image }: { image: any }) => z.object({
     draft: z.boolean().default(true),
     tags: z.array(z.enum(["SRE", "Frontend", "Platform Engineering"])),
     featuredimage: imageSchema({ image }).optional(),
-    heroimage: image().optional(),
+    heroimage: imageSchema({ image }).optional(),
 });
