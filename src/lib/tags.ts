4271bcd73c8de53a18fb9fae36b285946e7eddb5
kungfoome <421589+kungfoome@users.noreply.github.com>
2024-01-16 15:29:11 -0700

    Fix typescript types for tag count return object

--- src/lib/tags.ts
+++ src/lib/tags.ts
@@ -1,7 +1,8 @@
 import { getPublishedArticles } from '../lib/articles';
+import { Tags } from '../schemas/ArticleSchema';
 
 type TagInfo = {
-    tag: string;
+    tag: Tags;
     tagLink: string;
     count: number;
 };
@@ -14,14 +15,21 @@ export async function countTagsByArticles(): Promise<TagInfo[]> {
     const articles = await getPublishedArticles();
 
     const tagCounts = articles
-        .map(article => article.data.tags || [])
-        .flatMap(tags => tags)
-        .reduce((acc, tag) => {
-            acc[tag] = acc[tag] || { tag, tagLink: getFormattedTag(tag), count: 0 };
-            acc[tag].count++;
-            return acc;
-        }, {} as Record<string, TagInfo>);
-    
+        .map((article) => article.data.tags || [])
+        .flatMap((tags) => tags)
+        .reduce(
+            (acc, tag) => {
+                acc[tag] = acc[tag] || {
+                    tag,
+                    tagLink: getFormattedTag(tag),
+                    count: 0
+                };
+                acc[tag].count++;
+                return acc;
+            },
+            {} as Record<string, TagInfo>
+        );
+
     // Sort by count
     return Object.values(tagCounts).sort((a, b) => b.count - a.count);
 }
